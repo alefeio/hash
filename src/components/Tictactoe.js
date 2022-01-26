6bb4d12692e2526046d2806e27db92ece3ac8441
Alexandre Feio <alexandre.feio@bussoladoinvestidor.com>
2022-01-26 02:09:48 -0300

    correção de bug e organização de código

--- src/components/Tictactoe.js
+++ src/components/Tictactoe.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, Fragment } from "react";
 
 function Tictactoe() {
   const [player, setPlayer] = useState(1);
@@ -11,62 +11,7 @@ function Tictactoe() {
   const [styleLine, setStyleline] = useState('');
   const [start, setStart] = useState(false);
 
-  function handlePosition(cell, type) {
-    const obj = position;
-
-    const objIndex = obj.findIndex(
-      (obj) => obj.cell === cell
-    );
-
-    obj[objIndex].type = type;
-
-    setPosition(obj);
-  }
-
-  function alterTurn() {
-    player === 1 ? setPlayer(2) : setPlayer(1);
-    turn === 'x' ? setTurn('o') : setTurn('x');
-  }
-
-  function play(cell) {
-    const array = [cell];
-
-    if (player === 1) {
-      setPlayer1([...player1, cell]);
-      player1.map(p => array.push(p));
-    } else {
-      setPlayer2([...player2, cell]);
-      player2.map(p => array.push(p));
-    }
-
-    array.length >= 3 && verify(array);
-
-    alterTurn();
-  }
-
-  function verify(arr) {
-    console.log('verify')
-    const numberOfMoves = position.filter(p => p.type === '');
-    let r = []
-    correctanswers.map(res => {
-      r = res.filter(r => arr.includes(r));
-
-      if (r.length >= 3) {
-        setVictory(`Jogador ${player} venceu!`);
-        r[0] === 0 && r[1] === 1 && r[2] === 2 && setStyleline('success');
-        r[0] === 3 && r[1] === 4 && r[2] === 5 && setStyleline('success2');
-        r[0] === 6 && r[1] === 7 && r[2] === 8 && setStyleline('success3');
-        r[0] === 0 && r[1] === 3 && r[2] === 6 && setStyleline('success4');
-        r[0] === 1 && r[1] === 4 && r[2] === 7 && setStyleline('success5');
-        r[0] === 2 && r[1] === 5 && r[2] === 8 && setStyleline('success6');
-        r[0] === 0 && r[1] === 4 && r[2] === 8 && setStyleline('success7');
-        r[0] === 2 && r[1] === 4 && r[2] === 6 && setStyleline('success8');
-        return false;
-      }
-      !numberOfMoves.length && setVictory('Deu velha!');
-    });
-  }
-
+  // start the game
   function getStart() {
     setPosition([
       { cell: 0, type: '' },
@@ -88,6 +33,71 @@ function Tictactoe() {
     setStart(false)
   }
 
+  // save player steps
+  function play(cell) {
+    const array = [];
+
+    if (player === 1) {
+      setPlayer1([...player1, cell]);
+      player1.map(p => array.push(p));
+    } else {
+      setPlayer2([...player2, cell]);
+      player2.map(p => array.push(p));
+    }
+
+    array.push(cell);
+
+    array.length >= 3 && verify(array);
+
+    alterTurn();
+  }
+
+  // fill the cell with the symbol
+  function handlePosition(cell, type) {
+    const obj = position;
+
+    const objIndex = obj.findIndex(
+      (obj) => obj.cell === cell
+    );
+
+    obj[objIndex].type = type;
+
+    setPosition(obj);
+  }
+
+  // toggles player and symbol
+  function alterTurn() {
+    player === 1 ? setPlayer(2) : setPlayer(1);
+    turn === 'x' ? setTurn('o') : setTurn('x');
+  }
+
+  // checks if there is a win or a draw
+  function verify(arr) {
+    console.log('verify')
+    const numberOfMoves = position.filter(p => p.type === '');
+    let condition = false;
+    correctanswers.map(res => {
+      const r = res.filter(r => arr.includes(r));
+
+      if (r.length === 3) condition = true;
+
+      if (condition) {
+        r[0] === 0 && r[1] === 1 && r[2] === 2 && setStyleline('success');
+        r[0] === 3 && r[1] === 4 && r[2] === 5 && setStyleline('success2');
+        r[0] === 6 && r[1] === 7 && r[2] === 8 && setStyleline('success3');
+        r[0] === 0 && r[1] === 3 && r[2] === 6 && setStyleline('success4');
+        r[0] === 1 && r[1] === 4 && r[2] === 7 && setStyleline('success5');
+        r[0] === 2 && r[1] === 5 && r[2] === 8 && setStyleline('success6');
+        r[0] === 0 && r[1] === 4 && r[2] === 8 && setStyleline('success7');
+        r[0] === 2 && r[1] === 4 && r[2] === 6 && setStyleline('success8');
+        setVictory(`Jogador ${player} venceu!`);
+      } else {
+        !numberOfMoves.length && !victory && setVictory('Deu velha!');
+      }
+    });
+
+  }
+
   useEffect(() => {
     getStart();
     setCorrectanswers([
@@ -104,28 +114,7 @@ function Tictactoe() {
 
   return (
     <div className="App-header">
-      {victory &&
-        <div className="end">
-          <h1>{victory}</h1>
-          <button className="btn1" onClick={() => getStart()}>Jogar novamente</button>
-        </div>
-      }
-      {start ? <div className="grid-board">
-        {styleLine && <span className={styleLine}></span>}
-        {position.map((pos, index) => (
-          pos.type
-            ?
-            <div key={index}><img src={`assets/images/${pos.type}.svg`} /></div>
-            :
-            <div key={index}>
-              {!victory && <button
-                onClick={() => { handlePosition(index, turn); play(index) }}
-              >
-                {' '}
-              </button>}
-            </div>
-        ))}
-      </div> :
+      {!start ?
         <div className="start">
           <h1>Escolha para começar:</h1>
           <div>
@@ -134,6 +123,31 @@ function Tictactoe() {
             <img src="assets/images/x.svg" onClick={() => { setTurn('x'); setStart(true) }} />
           </div>
         </div>
+        :
+        <Fragment>
+          {victory &&
+            <div className="end">
+              <h1>{victory}</h1>
+              <button className="btn1" onClick={() => getStart()}>Jogar novamente</button>
+            </div>
+          }
+          <div className="grid-board">
+            {styleLine && <span className={styleLine}></span>}
+            {position.map((pos, index) => (
+              pos.type
+                ?
+                <div key={index}><img src={`assets/images/${pos.type}.svg`} /></div>
+                :
+                <div key={index}>
+                  {!victory && <button
+                    onClick={() => { handlePosition(index, turn); play(index) }}
+                  >
+                    {' '}
+                  </button>}
+                </div>
+            ))}
+          </div>
+        </Fragment>
       }
     </div>
   );
